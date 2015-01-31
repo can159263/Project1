a52fb43b379183a0863bdc83b47db98a19408ad4
tomcan <tomcan@yeah.net>
2015-01-31 11:13:40 +0800

    测试版本导入2015-1-31

---
+++ TestProject1/src/com/tomcan/component/TestMain.java
@@ -0,0 +1,10 @@
+package com.tomcan.component;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 好玩的东西
+	}
+
+}
---
+++ TestProject1/src/com/tomcan/component/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author lenovo
+ *
+ */
+package com.tomcan.component;
